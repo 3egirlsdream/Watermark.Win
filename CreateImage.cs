b797269064451a4e0f0e0b9dd0c50ef8715616a7
3egirlsdream <xulezuo@hotmail.com>
2022-07-19 15:28:41 +0800

    commit

---
+++ CreateImage.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JointWatermark
+{
+    internal class CreateImage
+    {
+        public static Task CreatePic(int width, int height)
+        {
+           return  Task.Run(() =>
+            {
+                var hei = height * 0.13;
+                Bitmap bmp = new Bitmap(width, (int)hei);                      //改图只显示最近输入的700个点的数据曲线。
+                                                                               //  Graphics graphics = Graphics.FromImage(bmp);
+                                                                               //   SolidBrush brush1 = new SolidBrush(Color.FromArgb(255, 0, 0));
+                                                                               //   graphics.FillRectangle(brush1, 0, 0, 700, 550);//Brushes.Sienna
+                for (int i = 0; i < bmp.Width; i++)
+                    for (int j = 0; j < bmp.Height; j++)
+                    {
+                        Color c = Color.FromArgb(255, 255, 255);
+                        bmp.SetPixel(i, j, c);
+                    }
+                bmp.Save("watermark.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);//指定图片格式   
+                bmp.Dispose();
+            });
+            
+        }
+
+
+        public static Task AddWaterMarkImg(string sFile, string dFile, string waterFile, DateTime datetime, string deviceName, Bitmap map1, Tuple<int, int> tuple, bool preview, string mount, string xy, double scale = 1, double opacity = 0.8, int locationX = -1, int locationY = -1)
+        {
+            return Task.Run(() =>
+            {
+                Bitmap sourceImage = new Bitmap(sFile);
+                Bitmap waterImage = new Bitmap(waterFile);
+
+                Bitmap bitmap = new Bitmap(sourceImage, sourceImage.Width, sourceImage.Height);
+                Graphics g = Graphics.FromImage(bitmap);
+                double xs = (double)(sourceImage.Height / 2) / waterImage.Height;
+                float waterWidth = (float)(waterImage.Width * xs);
+                float waterHeight = (float)(waterImage.Height * xs);
+
+                //下面定义一个矩形区域      
+                float rectWidth = waterWidth;
+                float rectHeight = waterHeight;
+
+                if (locationX == -1) locationX = (int)(((double)2 / 3) * sourceImage.Width - rectWidth);
+                if (locationY == -1) locationY = (int)(0.5 * sourceImage.Height - 0.5 * rectHeight);
+                //声明矩形域
+                RectangleF textArea = new RectangleF(locationX, locationY, rectWidth, rectHeight);
+                Bitmap w_bitmap = ChangeOpacity(waterImage, scale, opacity);
+
+                //字体比例
+                double fontxs = ((double)sourceImage.Height / 156);
+                if (fontxs < 1) fontxs = 1;
+
+                //划线
+                g.DrawLine(new Pen(Color.LightGray, (int)(2 * fontxs)), new Point(locationX + (int)rectWidth + 10, (int)(0.8 * sourceImage.Height)), new Point(locationX + (int)rectWidth + 10, (int)(0.3 * sourceImage.Height)));
+
+                //写字
+                var font = new Font("微软雅黑", (int)(25 * fontxs), FontStyle.Bold);
+                var brush = new SolidBrush(Color.Black);
+                var point = new Point(locationX + (int)rectWidth + 50, (int)(0.3 * sourceImage.Height));
+                g.DrawString(mount, font, brush, point);
+
+
+                font = new Font("微软等线Light", (int)(20 * fontxs), FontStyle.Regular);
+                var c = ColorTranslator.FromHtml("#919191");
+                brush = new SolidBrush(c);
+                point = new Point(locationX + (int)rectWidth + 50, (int)(0.6 * sourceImage.Height));
+                g.DrawString(xy, font, brush, point);
+
+                //画时间
+                font = new Font("微软等线Light", (int)(20 * fontxs), FontStyle.Regular);
+                c = ColorTranslator.FromHtml("#919191");
+                brush = new SolidBrush(c);
+                point = new Point(100, (int)(0.6 * sourceImage.Height));
+                g.DrawString(datetime.ToString("yyyy.MM.dd HH:mm:ss"), font, brush, point);
+
+                //画设备
+                font = new Font("微软雅黑", (int)(28 * fontxs), FontStyle.Bold);
+                brush = new SolidBrush(Color.Black);
+                point = new Point(100, (int)(0.25 * sourceImage.Height));
+                g.DrawString(deviceName, font, brush, point);
+
+                g.DrawImage(w_bitmap, textArea);
+                bitmap.Save("temp_" + dFile);
+
+                if (preview)
+                {
+                    waterImage.Dispose();
+                    w_bitmap.Dispose();
+                    bitmap.Dispose();
+                    g.Dispose();
+                    sourceImage.Dispose();
+                    return;
+                }
+
+                var map2 = new Bitmap("temp_" + dFile);
+
+                //拼接图片
+                var w = tuple.Item1;
+                var h = tuple.Item2 + sourceImage.Height;
+                RectangleF area = new RectangleF(0, 0, rectWidth, h);
+                var _bitmap = new Bitmap(w, h, PixelFormat.Format24bppRgb);
+                _bitmap.SetResolution(96.0F, 96.0F); // 重点
+                                                     //_bitmap.SetResolution(300, 300);
+                Graphics _g = Graphics.FromImage(_bitmap);
+                _g.FillRectangle(Brushes.White, new Rectangle(0, 0, w, h));
+                _g.DrawImage(map1, 0, 0, w, map1.Height);
+                map1.SetResolution(72, 72);
+                _g.DrawImage(map2, 0, map1.Height, w, map2.Height);
+                map2.SetResolution(72, 72);
+
+                dFile = $@"{MainWindow.binpath}\\{dFile}";
+                //保存图片
+                _bitmap.Save(dFile);
+                g.Dispose();
+                return;
+            });
+
+        }
+
+        /// <summary>
+        /// 改变图片的透明度
+        /// </summary>
+        /// <param name="img">图片</param>
+        /// <param name="opacityvalue">透明度</param>
+        /// <returns></returns>
+        private static Bitmap ChangeOpacity(Image waterImg, double scale, double opacityvalue)
+        {
+
+            float[][] nArray ={ new float[] {1, 0, 0, 0, 0},
+
+                                new float[] {0, 1, 0, 0, 0},
+
+                                new float[] {0, 0, 1, 0, 0},
+
+                                new float[] {0, 0, 0, (float)opacityvalue, 0},
+
+                                new float[] {0, 0, 0, 0, 1}};
+
+            ColorMatrix matrix = new ColorMatrix(nArray);
+
+            ImageAttributes attributes = new ImageAttributes();
+
+            attributes.SetColorMatrix(matrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
+
+            var waterWidth = (int)(waterImg.Width * scale);
+            var waterHeight = (int)(waterImg.Height * scale);
+
+            //缩放水印图片
+            Bitmap waterBitMap = new Bitmap(waterWidth, waterHeight);
+            Graphics waterG = Graphics.FromImage(waterBitMap);
+            waterG.DrawImage(waterImg, 0, 0, waterWidth, waterHeight);
+
+
+            Bitmap resultImage = new Bitmap(waterWidth, waterHeight);
+            Graphics g = Graphics.FromImage(resultImage);
+            g.DrawImage(waterBitMap, new Rectangle(0, 0, waterWidth, waterHeight), 0, 0, waterWidth, waterHeight, GraphicsUnit.Pixel, attributes);
+
+            return resultImage;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,235 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Image = System.Drawing.Image;
+
+namespace JointWatermark
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static string logo = path+ "\\sony.png";
+        public static string binpath;
+        public static string path;
+        public static string sourceImgUrl;
+        public static string lastUrl;
+        public VM vm;
+        public MainWindow()
+        {
+            InitializeComponent();
+            vm = new VM();
+            this.DataContext = vm;
+            binpath = Environment.CurrentDirectory; ;
+            path = binpath.Substring(0, binpath.IndexOf("bin"));
+            logo = path + "\\sony.png";
+            xy.Text = "44°29′12\"E 33°23′46\"W";
+        }
+
+        private void SelectPictureClick(object sender, RoutedEventArgs e)
+        {
+            // 实例化一个文件选择对象
+            Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.DefaultExt = ".png";  // 设置默认类型
+                                         // 设置可选格式
+            dialog.Filter = @"图像文件(*.jpg,*.png)|*jpeg;*.jpg;*.png
+      |JPEG(*.jpeg, *.jpg)|*.jpeg;*.jpg|PNG(*.png)|*.png";
+            // 打开选择框选择
+            Nullable<bool> result = dialog.ShowDialog();
+            if (result == true)
+            {
+                BitmapImage bitmap = new BitmapImage(new Uri(dialog.FileName, UriKind.Absolute));
+                sourceImg.Source = bitmap;// ; // 获取选择的文件名
+                sourceImgUrl = dialog.FileName;
+            }
+        }
+
+
+        private void Card_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is MaterialDesignThemes.Wpf.Card card && card.Tag is string tag)
+            {
+                logo = path + "\\" + tag + ".png";
+                if(!string.IsNullOrEmpty(sourceImgUrl))
+                    SetPreviewImg();
+            }
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            vm.Loading = true;
+            Bitmap sourceImage = new Bitmap(sourceImgUrl);
+            var img = Image.FromFile(sourceImgUrl);
+            DateTime datetime;
+            try
+            {
+                var dt = img.GetPropertyItem(0x0132).Value;
+                var dateTimeStr = System.Text.Encoding.ASCII.GetString(dt).Trim('\0');
+                datetime = DateTime.ParseExact(dateTimeStr, "yyyy:MM:dd HH:mm:ss", CultureInfo.InvariantCulture);
+            }
+            catch (Exception ex)
+            {
+                datetime = DateTime.Now;
+            }
+
+            var Width = sourceImage.Width;
+            var Height = sourceImage.Height;
+            try
+            {
+                await CreateImage.CreatePic(Width, Height);
+                var c = Tuple.Create(Width, Height);
+                var dFileName = $"{DateTime.Now.ToString("yyyyMMddHHmmss")}.jpg";
+                await CreateImage.AddWaterMarkImg($@"{binpath}\watermark.jpg", dFileName, $@"{logo}", datetime, deviceName.Text, sourceImage, c, false, mount.Text, xy.Text, 1, 1);
+                lastUrl = binpath + "\\" + dFileName;
+                BitmapImage bitmap2 = new BitmapImage(new Uri(lastUrl, UriKind.Absolute));
+                createdImg.Source = bitmap2;
+                var previewUrl = binpath + "\\temp_" + dFileName;
+                BitmapImage previwMap = new BitmapImage(new Uri(previewUrl, UriKind.Absolute));
+                preveiew.Source = previwMap;
+            }
+            catch(Exception ex)
+            {
+                SnackbarThree.IsActive = true;
+                Task.Run(() =>
+                {
+                    Thread.Sleep(2000);
+                    Dispatcher.Invoke(() =>
+                    {
+                        SnackbarThree.IsActive = false;
+                    });
+                });
+            }
+
+            vm.Loading = false;
+        }
+
+        private void downloadClick(object sender, RoutedEventArgs e)
+        {
+            var dlg = new SaveFileDialog()
+            {
+                Title = "另存为",
+                DefaultExt = "jpg",
+                Filter = "Jpg files (*.jpg)|*.jpg|All files|*.*",
+            };
+            if (dlg.ShowDialog() == true)
+            {
+
+                Bitmap sourceImage = new Bitmap(lastUrl);
+                sourceImage.Save(dlg.FileName);
+            }
+        }
+
+        private async void SetPreviewImg()
+        {
+            try
+            {
+                Bitmap sourceImage = new Bitmap(sourceImgUrl);
+                var img = Image.FromFile(sourceImgUrl);
+                DateTime datetime;
+                try
+                {
+                    var dt = img.GetPropertyItem(0x0132).Value;
+                    var dateTimeStr = System.Text.Encoding.ASCII.GetString(dt).Trim('\0');
+                    datetime = DateTime.ParseExact(dateTimeStr, "yyyy:MM:dd HH:mm:ss", CultureInfo.InvariantCulture);
+                }
+                catch (Exception ex)
+                {
+                    datetime = DateTime.Now;
+                }
+
+                var Width = sourceImage.Width;
+                var Height = sourceImage.Height;
+
+                await CreateImage.CreatePic(Width, Height);
+                var c = Tuple.Create(Width, Height);
+                var dFileName = $"{DateTime.Now.ToString("yyyyMMddHHmmss")}.jpg";
+                await CreateImage.AddWaterMarkImg($@"{binpath}\watermark.jpg", dFileName, $@"{logo}", datetime, deviceName.Text, sourceImage, c, true, mount.Text, xy.Text, 1, 1);
+                var previewUrl = binpath + "\\temp_" + dFileName;
+                BitmapImage previwMap = new BitmapImage(new Uri(previewUrl, UriKind.Absolute));
+                preveiew.Source = previwMap;
+            }
+            catch (Exception ex)
+            {
+                SnackbarThree.IsActive = true;
+                Task.Run(() =>
+                {
+                    Thread.Sleep(2000);
+                    Dispatcher.Invoke(() =>
+                    {
+                        SnackbarThree.IsActive = false;
+                    });
+                });
+            }
+        }
+
+        private void Card_MouseLeftButtonDown_1(object sender, MouseButtonEventArgs e)
+        {
+
+            // 实例化一个文件选择对象
+            Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.DefaultExt = ".png";  // 设置默认类型
+                                         // 设置可选格式
+            dialog.Filter = @"图像文件(*.png)|*.png";
+            // 打开选择框选择
+            Nullable<bool> result = dialog.ShowDialog();
+            if (result == true)
+            {
+                BitmapImage bitmap = new BitmapImage(new Uri(dialog.FileName, UriKind.Absolute));
+                selfmade.Source = bitmap;// ; // 获取选择的文件名
+                selfmade.Visibility = Visibility.Visible;
+                plus.Visibility = Visibility.Collapsed;
+                logo = dialog.FileName;
+            }
+        }
+
+        private void deviceName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(!string.IsNullOrEmpty(sourceImgUrl))
+                SetPreviewImg();
+        }
+    }
+
+    public class VM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        public VM()
+        {
+
+        }
+
+        private bool loading = false;
+        public bool Loading
+        {
+            get => loading;
+            set
+            {
+                loading = value;
+                NotifyPropertyChanged(nameof(Loading));
+            }
+        }
+    }
+}
