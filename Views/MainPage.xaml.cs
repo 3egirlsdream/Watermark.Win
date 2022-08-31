f39efcae70b25a623b2fcb91e089a99f36b0a86d
XuLezuo <xulezuo@hotmail.com>
2022-08-31 22:40:17 +0800

    commit

--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -390,6 +390,99 @@ namespace JointWatermark
                 ((MainWindow)(App.Current.MainWindow)).SendMsg(ex.Message);
             }
         }
+
+
+        private bool mouseDown;
+        private System.Windows.Point mouseXY;
+        private double min = 0.1, max = 3.0;//最小/最大放大倍数
+
+        private void ContentControl_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var img = sender as ContentControl;
+            if (img == null)
+            {
+                return;
+            }
+            img.CaptureMouse();
+            mouseDown = true;
+            mouseXY = e.GetPosition(img);
+        }
+
+        private void ContentControl_MouseMove(object sender, MouseEventArgs e)
+        {
+            var img = sender as ContentControl;
+            if (img == null)
+            {
+                return;
+            }
+            if (mouseDown)
+            {
+                Domousemove(img, e);
+            }
+        }
+
+        private void ContentControl_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            var img = sender as ContentControl;
+            if (img == null)
+            {
+                return;
+            }
+            var point = e.GetPosition(img);
+            var group = createdImg.FindResource("TfGroup") as TransformGroup;
+            var delta = e.Delta * 0.001;
+            DowheelZoom(group, point, delta);
+        }
+
+        private void ContentControl_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            var img = sender as ContentControl;
+            if (img == null)
+            {
+                return;
+            }
+            img.ReleaseMouseCapture();
+            mouseDown = false;
+        }
+
+        private void Domousemove(ContentControl img, MouseEventArgs e)
+        {
+            if (e.LeftButton != MouseButtonState.Pressed)
+            {
+                return;
+            }
+            var group = createdImg.FindResource("TfGroup") as TransformGroup;
+            var transform = group.Children[1] as TranslateTransform;
+            var position = e.GetPosition(img);
+            transform.X -= mouseXY.X - position.X;
+            transform.Y -= mouseXY.Y - position.Y;
+            mouseXY = position;
+        }
+
+        private void ResetImagePositionClick(object sender, RoutedEventArgs e)
+        {
+            var group = createdImg.FindResource("TfGroup") as TransformGroup;
+            var transform = group.Children[1] as TranslateTransform;
+            transform.X = 0;
+            transform.Y = 0;
+            var transform2 = group.Children[0] as ScaleTransform;
+            transform2.ScaleX = 1;
+            transform2.ScaleY = 1;
+        }
+
+        private void DowheelZoom(TransformGroup group, System.Windows.Point point, double delta)
+        {
+            var pointToContent = group.Inverse.Transform(point);
+            var transform = group.Children[0] as ScaleTransform;
+            if (transform.ScaleX + delta < min) return;
+            if (transform.ScaleX + delta > max) return;
+            transform.ScaleX += delta;
+            transform.ScaleY += delta;
+            var transform1 = group.Children[1] as TranslateTransform;
+            transform1.X = -1 * ((pointToContent.X * transform.ScaleX) - point.X);
+            transform1.Y = -1 * ((pointToContent.Y * transform.ScaleY) - point.Y);
+        }
+
     }
 
     public class MainVM : ValidationBase
