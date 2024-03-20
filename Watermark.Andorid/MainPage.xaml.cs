bffa858f05f83fe8d4e251c0cc2adfd894f4b627
XuLezuo <xulezuo@hotmail.com>
2024-03-20 17:09:09 +0800

    commit

--- Watermark.Andorid/MainPage.xaml.cs
+++ Watermark.Andorid/MainPage.xaml.cs
@@ -1,17 +1,20 @@
-﻿namespace Watermark.Andorid
+﻿using Microsoft.AspNetCore.Components.WebView;
+
+namespace Watermark.Andorid
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
-        }
+		}
 
-        private void blazorWebView_BlazorWebViewInitialized(object sender, Microsoft.AspNetCore.Components.WebView.BlazorWebViewInitializedEventArgs e)
+		private void blazorWebView_BlazorWebViewInitialized(object sender, Microsoft.AspNetCore.Components.WebView.BlazorWebViewInitializedEventArgs e)
         {
 #if ANDROID
             e.WebView.Settings.MixedContentMode = Android.Webkit.MixedContentHandling.AlwaysAllow;
+			e.WebView.VerticalScrollBarEnabled = false;
 #endif
-        }
+		}
     }
 }
--- Watermark.Andorid/Platforms/Android/MainActivity.cs
+++ Watermark.Andorid/Platforms/Android/MainActivity.cs
@@ -1,11 +1,23 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Views;
+using Color = Android.Graphics.Color;
 
 namespace Watermark.Andorid
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
-    }
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+#if ANDROID
+			//设置状态栏，导航样颜色为透明
+			Window.SetStatusBarColor(Color.Transparent);
+			Window.SetNavigationBarColor(Color.Transparent);
+			Window.DecorView.SystemUiVisibility = (StatusBarVisibility)SystemUiFlags.LightStatusBar;
+#endif
+			base.OnCreate(savedInstanceState);
+		}
+	}
 }
