bc03781dc27ebb5d89c87076d2d163e4a7c5bdc1
XuLezuo <xulezuo@hotmail.com>
2024-04-10 11:21:00 +0800

    commit

--- Watermark.Shared/Models/WMLoginModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Watermark.Win.Models
-{
-    public class WMLoginModel
-    {
-        public string Message { get; set; }
-        public WMLoginChildModel data { get; set; }
-        public string token { get; set; }
-    }
-}
