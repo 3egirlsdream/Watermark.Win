b37a85c31b209ab9ea9df179e9a505a1a2250b44
XuLezuo <xulezuo@hotmail.com>
2024-05-31 17:25:52 +0800

    swiper

---
+++ Watermark.Andorid/wwwroot/js/init-swiper.js
@@ -0,0 +1,17 @@
+﻿function swiperInit(dotNetCallbackRef, callbackMethod, id, index) {
+    console.log('Entered initSwiper!');
+    let className = "." + id;
+    window[id] = new Swiper(className, {
+        observer: true,
+        observeParents: true,
+        observeSlideChildren: true,
+        autoHeight: true,
+        initialSlide: index,
+        on: {
+            slideChangeTransitionStart: function () {
+                dotNetCallbackRef.invokeMethodAsync(callbackMethod, this.activeIndex);
+                //alert(this.activeIndex);//切换结束时，告诉我现在是第几个slide
+            },
+        }
+    });
+}
