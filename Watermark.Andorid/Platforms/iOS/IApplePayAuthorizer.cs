c3b29b0760441c2aa7c94b4a29c6aae301fe1777
XuLezuo <xulezuo@hotmail.com>
2024-06-14 17:40:49 +0800

    commit

---
+++ Watermark.Andorid/Platforms/iOS/IApplePayAuthorizer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Watermark.Andorid.Platforms.iOS
+{
+    public interface IApplePayAuthorizer
+    {
+        void AuthorizePayment(double amount);
+        Action<string> PaymentSuccess { get; set; }
+        Action PaymentFailed { get; set; }
+    }
+}
